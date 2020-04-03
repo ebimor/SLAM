975df6863a6b9eaeb982ce74b92908f3975d6361
Ebrahim <eshahrivar@clearpath.ai>
2020-04-03 16:36:01 -0400

    landmark to pose edges

--- lsslam_framework/octave/create_g2o.m
+++ lsslam_framework/octave/create_g2o.m
@@ -13,7 +13,7 @@ addpath('tools');
 
 % real-world datasets
 %load ../data/intel.dat
-load ../data/intel.dat
+load ../data/dlr.dat
 
 fileID = fopen('data.g2o','w');
 N = (length(g.x)/3)-1
@@ -29,19 +29,36 @@ for eid = 1:length(g.edges)
   
   if (strcmp(edge.type, 'P') != 0)
   
-    if (!vertices(edge.fromIdx))
-      edge.fromIdx
-      fprintf(fileID,'VERTEX_SE2 %d %f %f %f \r\n',edge.fromIdx,g.x(edge.fromIdx:edge.fromIdx+2));
-      vertices(edge.fromIdx)=1;
-    end
-    
-    if (!vertices(edge.toIdx))
-      fprintf(fileID,'VERTEX_SE2 %d %f %f %f \r\n',edge.toIdx,g.x(edge.toIdx:edge.toIdx+2));
-      vertices(edge.toIdx)=1;
-    end
+      if (!vertices(edge.fromIdx))
+        fprintf(fileID,'VERTEX_SE2 %d %f %f %f \r\n',edge.fromIdx,g.x(edge.fromIdx:edge.fromIdx+2));
+        vertices(edge.fromIdx)=1;
+      end
+      
+      if (!vertices(edge.toIdx))
+        fprintf(fileID,'VERTEX_SE2 %d %f %f %f \r\n',edge.toIdx,g.x(edge.toIdx:edge.toIdx+2));
+        vertices(edge.toIdx)=1;
+      end
+      
+      I_triangular = [edge.information(1,1), edge.information(1,2), edge.information(1,3), edge.information(2,2), edge.information(2,3), edge.information(3,3)];
+      fprintf(fileID, 'EDGE_SE2 %d %d %f %f %f %f %f %f %f %f %f \r\n', edge.fromIdx, edge.toIdx, edge.measurement ,I_triangular)
+   
+  elseif (strcmp(edge.type, 'L') != 0)
+  
+      if (!vertices(edge.fromIdx))
+        fprintf(fileID,'VERTEX_SE2 %d %f %f %f \r\n',edge.fromIdx,g.x(edge.fromIdx:edge.fromIdx+2));
+        vertices(edge.fromIdx)=1;
+      end
 
-    fprintf(fileID, 'EDGE_SE2 %d %d %f %f %f %f %f %f %f %f %f \r\n', edge.fromIdx, edge.toIdx, edge.information)
-   end
+      %the second pose landmark pose
+      if (!vertices(edge.toIdx))
+        fprintf(fileID,'VERTEX_XY %d %f %f \r\n',edge.toIdx,g.x(edge.toIdx:edge.toIdx+1));
+        vertices(edge.toIdx)=1;
+      end
+      
+      I_triangular = [edge.information(1,1), edge.information(1,2), edge.information(2,2)];
+      fprintf(fileID, 'EDGE_SE2_XY %d %d %f %f %f %f %f \r\n', edge.fromIdx, edge.toIdx, edge.measurement, I_triangular)
+      
+  end
  end 
 fclose(fileID);
 
